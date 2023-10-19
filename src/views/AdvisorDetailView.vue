b9946889ee9e330868037dce53a0a97d7e294cb7
nichakamol <nichakamol_sanghong@cmu.ac.th>
2023-10-19 18:57:55 +0700

    Add advisor form

--- src/views/AdvisorDetailView.vue
+++ src/views/AdvisorDetailView.vue
@@ -5,18 +5,9 @@
 <div class="flex">
   
 <div class="flex card w-96 bg-base-100 shadow-2xl mt-5">
-  <div class="flex justify-start ml-32">
-    <button class="btn-sm btn-circle px-1.5 ml-12 mt-2 bg-red-400 hover:bg-gray-500"><svg class="h-5 w-5 text-white text-center"  fill="none" viewBox="0 0 24 24" stroke="currentColor">
-  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z"/>
-</svg>
-    </button>
-    <button class="btn-sm btn-circle px-1.5 ml-2 mt-2 bg-red-400 hover:bg-gray-500"><svg class="h-5 w-5 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"/>
-    </svg>
-    </button>
-</div>
+
   <figure class="px-10 pt-7">
-    <img src="https://media.discordapp.net/attachments/1102521841416949850/1163518158435536966/f02bed84317f6e41a5b0118348173512.png?ex=653fddba&is=652d68ba&hm=b43f3c2e84b66e3e721f9854fd84d1fbc4b248e829fa4439b6654fa3931f158b&=&width=1016&height=1016" alt="Shoes" class="rounded-md" />
+          <img v-for="image in advisor?.images" :key="image" :src="image" alt="Shoes" class="rounded-md" />
   </figure>
   <div class="card-body mt-2">
     <h3 class="text-gray-500 "><span class="font-bold">Name:</span> <span class="text-gray-400">{{ advisor?.name }}</span></h3>
@@ -25,8 +16,17 @@
     <h3 class="text-gray-500 "><span class="font-bold">academicPosition: </span><span class=" text-gray-400">{{ advisor?.academicPosition }}</span></h3>
 
 
-
     <div class="card-actions justify-end">
+    </div>
+    <div class="flex justify-start ml-32">
+    <button class="btn btn-circle px-1.5 ml-12 mt-2 bg-gray-600 hover:bg-gray-400"><svg class="h-7 w-7 text-white text-center"  fill="none" viewBox="0 0 24 24" stroke="currentColor">
+  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z"/>
+</svg>
+    </button>
+    <button class="btn btn-circle px-1.5 ml-2 mt-2 bg-gray-600 hover:bg-gray-400"><svg class="h-7 w-7 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"/>
+    </svg>
+    </button>
     </div>
   </div>
 </div>
@@ -36,10 +36,10 @@
     <h2 class="card-title"> Student </h2>
     
 <div v-for="advisees in advisor?.advisees" :key="advisees.id" class="text-left">
-  <div class="flex items-center"> <!-- Use flex to align image and text -->
+  <div class="flex items-center"> 
     <figure class="mr-3">
-      <img src="https://media.discordapp.net/attachments/856454942964056074/1163567240147386491/1e0204b943c97c63bb6bc20698172552.png?ex=65400b70&is=652d9670&hm=048986c5401505c13826c348e4aa2b3a00e60707331ae510c00e072d8e4cfc17&=&width=537&height=646" class="w-20 h-20 rounded-full">
-    </figure>
+      <img  v-for="image in advisees?.images" :key="image" :src="image" class="w-12 h-15 rounded-full">
+    </figure><!-- Use flex to align image and text -->    
     <span class="inline-block w-full bg-white border border-gray-400 rounded-md px-3 py-2 text-sm text-gray-800 hover:bg-gray-100">
       <p class="text-sm mt-1 text-gray-400">{{ advisees.studentID }}</p>
       <span class="font-semibold text-lg">{{advisees.name}} {{advisees.surname}}</span>
@@ -47,13 +47,8 @@
   </div>
 </div>
 </div>
-
-    
-    </div>
-  </div>
-
-
-
+ </div>
+ </div>
 
 <!--   
 </div> -->
---
+++ src/views/AdvisorFormView.vue
@@ -0,0 +1,90 @@
+<template>
+    <!-- component -->
+    <div class="min-h-screen h-10 bg-white flex">
+      <div class="w-full max-w-6xl">
+          <div class="bg-white rounded shadow-xl">
+              <div class="px-8 py-4 text-gray-700 text-3xl font-bold border-b-2 border-red-400">Add Advisor</div>
+              <form @submit.prevent="saveAdvisor" name="advisor_application" id="advisor_application" action="" class="py-4 px-8">
+                  <div class="mb-4">
+                      <label class="block text-gray-600 text-sm font-bold mb-2" for="advisor_name">Advisor Name:</label>
+                      <input v-model="advisor.name" class="border rounded w-full py-1 px-3 text-gray-700 border-gray-300" type="text" name="advisor_name" id="advisor_name" placeholder="Enter Advisor Name">
+                  </div>
+                  <div class="mb-4">
+                      <label class="block text-gray-600 text-sm font-bold mb-2" for="advisor_name">Advisor Surname:</label>
+                      <input v-model="advisor.surname" class="border rounded w-full py-2 px-3 text-gray-700 border-gray-300" type="text" name="advisor_surname" id="advisor_surname" placeholder="Enter Advisor Surname">
+                  </div>
+                  <div class="mb-4">
+                      <label class="block text-gray-600 text-sm font-bold mb-2" for="course_name">Department:</label>
+                      <input v-model="advisor.department" class="border rounded w-full py-2 px-3 text-gray-700 border-gray-300" type="text" name="course_name" id="department" placeholder="Enter Your Department">
+                      <p id="error_creater_id"></p>
+                  </div>
+                  <div class="mb-4">
+                      <label class="block text-gray-600 text-sm font-bold mb-2" for="course_name">academicPosition:</label>
+                      <input v-model="advisor.academicPosition" class="border rounded w-full py-2 px-3 text-gray-700 border-gray-300" type="text" name="course_name" id="academicPosition" placeholder="Enter Your academicPosition">
+                      <p id="error_creater_id"></p>
+                  </div>
+                  <div class="mb-4">
+                      <label class="block text-gray-700 text-sm font-bold mb-2" for="course_name">Add Image:</label>
+                      <ImageUpload v-model="advisor.images" />
+                      <!-- <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="default_size">Default size</label>
+                <input class="block w-full mb-5 text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" id="default_size" type="file"> -->
+  
+                  </div>
+                  <div class="mb-4 w-20 ml-auto">
+                  <button type="submit" class="w-full bg-gradient-to-r from-red-500 to-orange-500 text-white py-2 rounded hover:bg-gradient-to-r hover:from-red-500 hover:to-orange-600 transition duration-300">ADD</button>
+                  </div>
+              </form>
+          </div>
+      </div>
+  </div>
+  
+  </template>
+  <script setup lang="ts">
+  import type { AdvisorInfo } from '@/advisor';
+  import { ref } from 'vue';
+  import AdvisorService from '@/services/AdvisorService';
+  import { useMessageStore } from '@/stores/message'
+  import { useRouter } from 'vue-router';
+  import ImageUpload from '@/components/ImageUpload.vue';
+  
+  const store = useMessageStore()
+  
+  const router = useRouter()
+  
+  const advisor = ref<AdvisorInfo>({
+    id: 0,
+    name: '',
+    surname: '',
+    department: '',
+    images: [],
+    academicPosition: '',
+    advisees: [{ id: 1,name: '',studentID:'',surname: ''}],
+  })
+//   const advisees = ref<AdviseesStudent[]>([])
+// StudentService.getStudent()
+//   .then((response) => {
+//     advisees.value = response.data
+//   })
+//   .catch(() => {
+//     router.push({ name: 'network-error' })
+//   })
+  function saveAdvisor() {
+      AdvisorService.saveAdvisor(advisor.value)
+      .then((response) => {
+        console.log(response.data)
+        router.push({
+          name: 'advisor',
+          params: { id: response.data.id }
+        })
+        store.updateMessage('You are successfully add a new event for' + response.data.name)
+        setTimeout(() => {
+          store.resetMessage()
+        }, 3000)
+      })
+      .catch(() => {
+        router.push({ name: 'network-error' })
+      })
+  }
+  
+  
+  </script>
